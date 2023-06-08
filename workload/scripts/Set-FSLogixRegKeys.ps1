417573cac64663da89ead548f5fee0d0d57d7343
Jonathan Mesa <53017957+jonathanmesa@users.noreply.github.com>
2023-06-08 12:22:06 -0500

    Update Set-FSLogixRegKeys.ps1
    better comments and made the code more robust with logging

--- workload/scripts/Set-FSLogixRegKeys.ps1
+++ workload/scripts/Set-FSLogixRegKeys.ps1
@@ -1,13 +1,27 @@
-﻿param(
-    [string]$volumeshare
+# Define Parameters
+param(
+    [string]$volumeShare
 )
 
+# Set the registry path for FSLogix Profiles
+$registryPath = 'HKEY_LOCAL_MACHINE\Software\FSLogix\Profiles'
 
-reg.exe add 'HKEY_LOCAL_MACHINE\Software\FSLogix\Profiles' /v VHDLocations /t REG_MULTI_SZ /d $volumeshare /f
+# Set VHDLocations to the provided volume share
+Write-Host "Setting VHDLocations to $volumeShare"
+& reg.exe add $registryPath /v VHDLocations /t REG_MULTI_SZ /d $volumeShare /f
 
-reg.exe add 'HKEY_LOCAL_MACHINE\Software\FSLogix\Profiles' /v DeleteLocalProfileWhenVHDShouldApply /t REG_DWORD /d 1 /f
+# Set DeleteLocalProfileWhenVHDShouldApply to 1
+Write-Host "Enabling DeleteLocalProfileWhenVHDShouldApply"
+& reg.exe add $registryPath /v DeleteLocalProfileWhenVHDShouldApply /t REG_DWORD /d 1 /f
 
-reg.exe add 'HKEY_LOCAL_MACHINE\Software\FSLogix\Profiles' /v FlipFlopProfileDirectoryName /t REG_DWORD /d 1 /f
+# Set FlipFlopProfileDirectoryName to 1
+Write-Host "Enabling FlipFlopProfileDirectoryName"
+& reg.exe add $registryPath /v FlipFlopProfileDirectoryName /t REG_DWORD /d 1 /f
 
-reg.exe add 'HKEY_LOCAL_MACHINE\Software\FSLogix\Profiles' /v Enabled /t REG_DWORD /d 1 /f
+# Set Enabled to 1 to enable FSLogix Profiles
+Write-Host "Enabling FSLogix Profiles"
+& reg.exe add $registryPath /v Enabled /t REG_DWORD /d 1 /f
+
+# Output completion message
+Write-Host "FSLogix Profiles configuration completed."
 
